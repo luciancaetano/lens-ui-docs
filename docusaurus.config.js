d0363a466e4bfefa2cdec0a097cc468f1d9ab114
Lucian Caetano <lucian.santos.caetano@gmail.com>
2022-03-31 23:28:35 -0300

    build(settings):

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'luciancaetano', // Usually your GitHub org/user name.
   projectName: 'lens-ui', // Usually your repo name.
+  deploymentBranch: 'https://github.com/luciancaetano/lens-ui',
   themes: ['@docusaurus/theme-live-codeblock'],
 
   presets: [
